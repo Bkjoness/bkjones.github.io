bd2c1ea14470d074f2f222699fcca2c066a46c05
Ben Jones <bkjones817@gmail.com>
2020-12-09 23:50:03 +0000

    editing the sketch

--- draw.js
+++ draw.js
@@ -1,8 +1,5 @@
 let eyePosx = 100
 let eyePosy = 100
-let pupilPosx = 200
-let pupilPosy = 200
-
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
@@ -18,17 +15,17 @@ function draw() {
   for(let i = 0; i <= 14; i++) {
     for(let j = 0; j <= 12; j++) {
   fill(255);
-  ellipse(eyePosx * i + 200, eyePosy * j + 200, 100, 100);
+  ellipse(eyePosx * i + 100, eyePosy * j + 100, 100, 100);
       
       fill(mouseY, 0, 0, 50);
-  ellipse(eyePosx * i + 200, eyePosy * j + 200, 100, 100);
+  ellipse(eyePosx * i + 100, eyePosy * j + 100, 100, 100);
   
   
   pupilPosx = map(mouseX, 0, width, eyePosx - 20, eyePosx + 20, true);
   pupilPosy = map(mouseY, 0, height, eyePosy - 20, eyePosy + 20, true);
   
   fill(0);
-  ellipse(pupilPosx + 200 * i, pupilPosy + 200 * j, 30, 30);
+  ellipse(pupilPosx + 100 * i, pupilPosy + 100 * j, 30, 30);
     }
   }
 }
